8f38a4ffd047854a6917b156f4fdaaab60da5801
Jerzy Rozanski <jerzy.rozanski@outlook.com>
2013-10-16 13:25:12 +0200

    a msg

--- 06/selsort.cpp
+++ 06/selsort.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
- 
+
 using namespace std;
 
 typedef int node;
- 
+
 #define key(A) (A) 
 #define less(A, B) (key(A) < key(B))
 #define exch(A, B) {node t = A; A = B; B = t;}
@@ -23,28 +23,15 @@ int main() {
   selection_sort(v);
   return 0;
 }
- 
 
- 
-void selection_sort(vector<node>& v) {
- 
+void selection_sort(vector<node>& v) { 
   if ( v.empty() ) return;
- 
   int i, j, min;
- 
   for (i = 0; i < v.size(); i++) {
- 
     min = i;
- 
     for (j = i + 1; j < v.size(); j++)
- 
       if (less(v[j], v[min])) min = j;
- 
     exch(v[i], v[min]);
- 
   }
- 
   copy(v.begin(), v.end(), ostream_iterator<unsigned int>(cout, " ")); cout << endl;
- 
 }
- 
