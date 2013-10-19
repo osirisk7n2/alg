1a7cd96f7f4329ce2a5cf875dca5ffa1432a5b15
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-10-19 18:49:12 +0200

    a msg

--- 05/listyinc.cc
+++ 05/listyinc.cc
@@ -10,6 +10,7 @@ int main() {
   list<int> *listyinc = new list<int>[V+1];
   ifstream file;
   int **tab = new int *[E];
+
   for(int i = 0; i < E; ++i )
     tab[i] = new int [2];
   file.open("listyinc.in");
@@ -17,11 +18,14 @@ int main() {
     for(int j=0; j<2; ++j)
       file >> tab[i][j];
   file.close();
+
   for(int i=0; i<E; i++) {
     listyinc[tab[i][0]].push_back(tab[i][1]);
     listyinc[tab[i][1]].push_back(tab[i][0]);
   }
+
   for(list<int>::iterator it=listyinc[1].begin(); it !=listyinc[1].end(); ++it)
     cout << *it;
+
   delete[] listyinc;
 } 
