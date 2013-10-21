443c00e8feb429e65870be2eca4900dd8218a71c
JerzyRozanski <JerzyRozanski@gmail.com>
2013-10-21 21:47:39 +0200

    a msg

--- 06/qsort.cc
+++ 06/qsort.cc
@@ -17,33 +17,33 @@ int main() {
   vector<node> v;
   v.push_back('E');
 
-  for(int i=0; i<9; i++) {
-    v.insert(v.begin()+i, s[i]);
-  }; copy(v.begin(), v.end(), ostream_iterator<char>(cout, " ")); cout << endl;
+  for(int i=0; i<9; i++) v.insert(v.begin()+i, s[i]);
+  copy(v.begin(), v.end(), ostream_iterator<char>(cout, " ")); cout << endl;
 
   qsort(v, 0, v.size());
+  copy(v.begin(), v.end(), ostream_iterator<char>(cout, " ")); cout << endl;
 
   return 0;
 }
 
-int partition(vector<node>& v, int l, int r) {
-  int i = l-1, j = r;
-  node vi = v[r];
+int partition(vector<node>& v, int le, int ri) {
+  int i = le-1, j = ri;
+  node vi = v[ri];
   for (;;) {
     while (less(v[++i], vi)) ;
     while (less(vi, v[--j]))
-      if (j == l) break;
+      if (j == le) break;
     if (i >= j) break;
     exch(v[i], v[j]);
   }
-  exch(v[i], v[r]);
+  exch(v[i], v[ri]);
   return i;
 }
 
-void qsort(vector<node>& v, int l, int r) {
+void qsort(vector<node>& v, int le, int ri) {
   int pivot;
-  if (r <= l) return ;
-  pivot = partition(v, l, r);
-  qsort(v, l, pivot-1);
-  qsort(v, pivot+1, r);
+  if (ri <= le) return ;
+  pivot = partition(v, le, ri);
+  qsort(v, le, pivot-1);
+  qsort(v, pivot+1, ri);
 }
