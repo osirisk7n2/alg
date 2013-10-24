301e6677d87ba118ba18f4c3482d831ea1fba51b
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-10-24 14:42:29 +0200

    a msg

--- 13/spanntree.cc
+++ 13/spanntree.cc
@@ -5,38 +5,34 @@ bool *unvisited;
 list<int> *listyinc;
 list<pair<int, int> > tree;
 
-void dfsFind(int );
-list<pair<int, int> > spannTree() {
-  tree.clear();
-  dfsFind(1);
-  return tree;
+void dfs(int v) {
+  unvisited[v] = false;
+  for(list<int>::iterator it = listyinc[v].begin(); it != listyinc[v].end(); ++\
+	it) {
+    int u = *it;
+    if(unvisited[u]) {
+      tree.push_back(make_pair(v, u));
+      dfs(u);
+    }
+  }
 }
 
 int main() {
   xmpl();
   listyinc = new list<int>[V+1];
   unvisited = new bool[E+1];
-  for(int i=0;i<E+1;++i) unvisited[i] = true;
 
   for(int i=0; i<E; i++) {
     listyinc[tab[i][0]].push_back(tab[i][1]);
     listyinc[tab[i][1]].push_back(tab[i][0]);
   }
 
-  list<pair<int, int> > t=spannTree();
-  for(list<pair<int, int> >::iterator it=t.begin(); it!=t.end(); ++it)
+  for(int i=0;i<E+1;++i) unvisited[i] = true;
+  tree.clear();
+  dfs(2);
+  for(list<pair<int, int> >::iterator it=tree.begin(); it!=tree.end(); ++it)
 	cout<<it->first<<"-"<<it->second<<"\n";
 
-  delete[] listyinc, unvisited;
+  delete[] listyinc, unvisited, tab;
 } 
 
-void dfsFind(int v) {
-  unvisited[v] = false;
-  for(list<int>::iterator it = listyinc[v].begin(); it != listyinc[v].end(); ++it) {
-    int u = *it;
-    if(unvisited[u]) {
-      tree.push_back(make_pair(v, u));
-      dfsFind(u);
-    }
-  }
-}
