74afd8042c7ecc41701333f400c3910b0ecaa8bb
jerzro <jerzro@users.noreply.github.com>
2013-10-25 12:09:57 +0200

    Create tuples.cc

---
+++ 04/tuples.cc
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+#include <tuple>
+using namespace std;
+
+int main() {
+  typedef tuple<string, string> daneosobowe;
+  daneosobowe tab[10]= {make_tuple("Cathy", "Ball"),make_tuple ("Randy", "Crump")};
+}
