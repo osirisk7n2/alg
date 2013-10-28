016473cd273ea56a1b42d604f647d2194be31270
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-10-28 08:18:33 +0100

    a msg

--- 04/tuples.cc
+++ 04/tuples.cc
@@ -1,9 +1,10 @@
-#include <cstdlib>
+//#include <cstdlib>
 #include <iostream>
 #include <tuple>
 using namespace std;
 
 int main() {
   typedef tuple<string, string> daneosobowe;
-  daneosobowe tab[2]= {make_tuple("Cathy", "Ball"), make_tuple ("Randy", "Crump")};
+  daneosobowe tab[2]= {make_tuple("Cathy", "Ball"), make_tuple("Randy", "Crump")};
+  cout << get<0>(tab[1]) << " " << get<1>(tab[1]) << endl;
 }
