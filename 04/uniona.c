f60d0adac398af17a354a715a871e7d9c1995808
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-10-29 18:32:13 +0100

    a msg

--- 04/uniona.c
+++ 04/uniona.c
@@ -1,20 +1,28 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#define N 10
 
 struct point {
-  string nazwa;
+  char *nazwa;
   union {int x; int y;};
   struct point *next;
 };
+typedef struct point points;
 
 int main() {
-  point p1, p2, p3, *it;
-  p1.nazwa = "Warszawa";
-  p1.x = 1; p1.y = 2;
-  p1.next = &p2; p2.next = &p3;
+  points p[N], *it;
 
-  it = &p1;
-  while (it != &p3) it = it->next;
+  int i; for(i=0; i<N; ++i) {
+    p[i].nazwa = NULL; 
+    p[i].x = 0; p[i].y = 0; 
+    p[i].next = &p[i+1];
+  };
+  p[N].next = NULL;
+
+  it = &p[0];
+  while (it->next != NULL) {
+    printf("%i", it->x);
+    it = it->next;
+  }
 
   return 0;
 }
