40724dd1efc6eccdc4974068d30c09d7d6097d68
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-11-01 17:38:56 +0100

    a msg

--- 04/kl.cc
+++ 04/kl.cc
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int tab[]={1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
-class points
-{
+class points {
 public:
   int n;
   int distance();
-  points(int _n) { n = _n; x  = new int[n]; y = new int[n]; }
+  points(int _n, int* inx, int* iny);
   ~points(){ delete[] x, y; };
 private:
   int dist;
@@ -15,16 +13,26 @@ private:
 };
 
 int main() {
-  points Pkty (4);
+  int inx[] = {1, 2, 3, 4, 5, 6, 7};
+  int iny[] = {1, 2, 3, 4, 5, 6, 7};
+  points Pkty (4, inx, iny);
+
   cout << Pkty.distance()<<endl;
   return 0;
 }
 
 int points::distance() {
   dist = 0;
-  for (int i=0; i<2*n; i+=2) { 
-    x[i] = tab[i]; y[i] = tab[i+1];
-    dist = dist + x[i]*x[i] + y[i]*y[i];
+  for (int i=0; i<n; i++) {
+    dist = dist + y[i]*y[i] + x[i]*x[i];
   };
   return dist;
+}
+
+points::points(int _n, int *_inx, int *_iny) { 
+  n = _n;   
+  x  = new int[n]; y = new int[n];
+  for (int i=0; i<n; i++) {
+    x[i] = _inx[i]; y[i] = _iny[i];
+  }
 }
