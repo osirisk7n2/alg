a22eef7707ba809b6734243abb0140940b9a6ffb
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-11-01 10:00:06 +0100

    a msg

--- 04/kl.cc
+++ 04/kl.cc
@@ -1,29 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int tab[]={1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
 class points
 {
 public:
-  int distance(int );
+  int n;
+  int distance();
+  ~points(){ delete[] x, y;};
 private:
   int dist;
   int *x, *y;
 };
-int tab[]={1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
 
 int main() {
-  points p;
-  cout << p.distance(4)<<endl;
+  points W; W.n = 4;
+  cout << W.distance()<<endl;
   return 0;
 }
 
-int points::distance(int n) {
+int points::distance() {
   dist = 0;
   x  = new int[n]; y = new int[n];
   for (int i=0; i<2*n; i+=2) { 
     x[i] = tab[i]; y[i] = tab[i+1];
     dist = dist + x[i]*x[i] + y[i]*y[i];
   };
-  delete[] x, y;
   return dist;
 }
