4bb99378d07cc68e2d600defc54a381f0a827fc8
Jerzy Rozanski <jerzy_rozanski@sggw.pl>
2013-11-01 15:23:55 +0100

    a msg

--- 04/kl.cc
+++ 04/kl.cc
@@ -7,21 +7,21 @@ class points
 public:
   int n;
   int distance();
-  ~points(){ delete[] x, y;};
+  points(int _n) { n = _n; x  = new int[n]; y = new int[n]; }
+  ~points(){ delete[] x, y; };
 private:
   int dist;
   int *x, *y;
 };
 
 int main() {
-  points W; W.n = 4;
-  cout << W.distance()<<endl;
+  points Pkty (4);
+  cout << Pkty.distance()<<endl;
   return 0;
 }
 
 int points::distance() {
   dist = 0;
-  x  = new int[n]; y = new int[n];
   for (int i=0; i<2*n; i+=2) { 
     x[i] = tab[i]; y[i] = tab[i+1];
     dist = dist + x[i]*x[i] + y[i]*y[i];
