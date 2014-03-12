4434cd1a168fa5d6764942004f07770bf00e791b
Jerzy Rozanski <jerzy.rozanski@outlook.com>
2014-03-12 20:01:04 +0100

    uzycie fltk

--- 01/figury.cc
+++ 01/figury.cc
@@ -1,7 +1,11 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/fl_draw.H>
+// using namespace std;
 
+/*
 struct Punkt { int x1; int x2; };
 
 class Figura {
@@ -18,10 +22,30 @@ private:
   Figura(const Figura&);
   Figura& operator=(const Figura&);
 };
+*/
+
+class Rysunek : public Fl_Widget
+{
+  void draw( void );
+public:
+  Rysunek(int X,int Y,int W,int H) : Fl_Widget(X,Y,W,H) {}
+};
 
 
 int main()
 {
-    return 0;
+  Fl_Window window(300, 300, "Rysowanie");
+  Rysunek rysunek(0, 0, 300, 300);
+  window.end();
+  window.show();
+  return Fl::run();
 }
 
+
+void Rysunek::draw( void )
+{
+  fl_color( FL_BLACK );
+  fl_line_style( FL_DASH, 3 );
+  fl_line( 50, 50, 100, 100 );
+  fl_rect( 200, 200, 20, 50 );
+}
