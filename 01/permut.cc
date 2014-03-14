dc8bbbd0d62a305bf27302dc15758cb562b2352c
Jerzy Rozanski <jerzy.rozanski@outlook.com>
2014-03-14 17:14:45 +0100

    anylex

--- 01/permut.cc
+++ 01/permut.cc
@@ -3,35 +3,51 @@ using namespace std;
 
 int n;
 int *p;
-void generate(int , void (*f)(int[] )); 
-void write(int p[]);
+void (*pobranie)(int[]); // pobranie permutacji
+void odwrocenie(int ); // odwrocenie ciagu p[1], .., p[n]
+void generowanie(int , void (*f)(int[]) ); 
+void antyleks(int ); 
+void wypisz(int p[]);
 
 
 int main() {
   n = 3;
-  for (int i = 0; i < n; ++i) p[i] = i;
-  generate(n, write); 
+  generowanie(n, wypisz); 
   return 0;
 }
 
 
-void generate(int n, void (*f)(int[])) {
-  int t;
-  p = new int[n+1];
-  pobierz = f;
-
-
-  if (n == 1) 
-    write(p);
-
-  for (int i = 1; i < n; i++) { 
-    swap(p[i], p[n]);
-    generate(n-1, f);
-    swap(p[i], p[n]);
+void odwrocenie(int m) {
+  int i = 1, j = m;
+  while(i<j) {
+    swap(p[i], p[j]);
+    ++i; --j;
   }
 }
 
-void write(int p[]) {
-  for (int i = 1; i < n; ++i) cout << p[i] << ' ';
+void antyleks(int m) {
+  if (m == 1) pobranie(p); 
+  else {
+    for (int i=1; i<m+1; ++i) {
+      antyleks(m-1);
+      if (i < m) {
+	swap(p[i], p[m]);
+	odwrocenie(m-1);
+      }
+    }
+  }
+}
+
+void generowanie(int n, void (*f)(int[])) {
+  p = new int[n+1];
+  pobranie = f;
+  for (int i = 1; i < n+1; ++i) p[i] = i;
+  
+  antyleks(n);
+  delete[] p;
+}
+
+void wypisz(int p[]) {
+  for (int i = 1; i < n+1; ++i) cout << p[i] << ' ';
   cout << '\n';
 } 
