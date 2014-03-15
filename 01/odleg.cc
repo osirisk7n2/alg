44fe135d362776f38cd590362b879598702ffecd
Jerzy Rozanski <jerzy.rozanski@outlook.com>
2014-03-15 14:12:43 +0100

    distance

--- 01/odleg.cc
+++ 01/odleg.cc
@@ -4,10 +4,16 @@ using namespace std;
 
 int main()
 {
-  int a = 7;
-  int b = 77;
+    int x = 3;
+    int punkt[] = {88, -2, 5, 10, -11, -1, 9, -88};
+    int max=0, idx=0, i;
 
-  cout << DistFuncs::MyDistFuncs::d(a, b) << endl;
+    for (i=0; i < sizeof(punkt)/sizeof(int); ++i) 
+      if (DistFuncs::MyDistFuncs::d(x, punkt[i]) > max) {
+	max = DistFuncs::MyDistFuncs::d(x, punkt[i]); 
+	idx = i;
+      } 
 
-  return 0;
+    cout << punkt[idx] << endl;
+    return 0;
 }
