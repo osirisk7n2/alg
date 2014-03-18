8ed7adf9b40b7b7eb185a7444fe6ef2fee58d66c
Jerzy Rozanski <jerzy.rozanski@outlook.com>
2014-03-18 17:26:27 +0100

    2max

--- 01/odleg.cc
+++ 01/odleg.cc
@@ -4,16 +4,21 @@ using namespace std;
 
 int main()
 {
-    int x = 3;
-    int punkt[] = {88, -2, 5, 10, -11, -1, 9, -88};
-    int max=0, idx=0, i;
+  int x = 3; // ustalony punkt poczatkowy
+    int punkt[] = {94, -2, 5, 10, -11, -1, 9, -88};
+    int i, max;
+    int idx[2]; // dwa rozwiazania
+    max = 0;
 
-    for (i=0; i < sizeof(punkt)/sizeof(int); ++i) 
+    for (i=0; i < sizeof(punkt)/sizeof(int); ++i) // dla znalezienia max 
       if (DistFuncs::MyDistFuncs::d(x, punkt[i]) > max) {
 	max = DistFuncs::MyDistFuncs::d(x, punkt[i]); 
-	idx = i;
-      } 
+	idx[0] = i;
+      }; 
+    for (i=0; i < sizeof(punkt)/sizeof(int); ++i) // drugie rozwiazaniex
+      if (DistFuncs::MyDistFuncs::d(x, punkt[i]) == max) 
+	idx[1] = i;
 
-    cout << punkt[idx] << endl;
+    cout << punkt[idx[0]] << ' ' << punkt[idx[1]] << endl;
     return 0;
 }
