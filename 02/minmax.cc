f257d02be19d8741e3b5547048686267ece0a8fa
Jerzy Rozanski <jerzy.rozanski@outlook.com>
2014-03-23 19:04:10 +0100

    pes

--- 02/minmax.cc
+++ 02/minmax.cc
@@ -8,20 +8,19 @@ int main() {
   int in[MAXSIZE] = {0};
   int ile = 0;
   int min, max;
+  int cnt;
   
   ifstream test("in-minmax.txt");
   while (!test.eof())
     test >> in[ile++];  
   test.close();
   
-  max = min = in[0];
+  max = min = in[0]; cnt = 0;
   for(int i = 0; i < ile-1; i++) {
-    if(in[i] > max)
-      max = in[i];
-    if(in[i] < min)
-      min = in[i];
+    if(in[i] > max) { max = in[i]; cnt++;};
+    if(in[i] < min) { min = in[i]; cnt++;};
   }
-
-  cout << min << " " << max << endl;
+  
+  cout << cnt << endl;
   return 0;
 }
